8c6f67a82bb174f11565b51226226a730a9360c7
hbh7 <hbh7@hbh7.com>
2017-07-18 14:06:09 -0400

    It actually compiles now

--- src/c/button_click.c
+++ src/c/button_click.c
@@ -1,40 +1,48 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *text_layer_intro;
+static TextLayer *text_layer_emelia;
+static TextLayer *text_layer_hunter;
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+  text_layer_set_text(text_layer_intro, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+  text_layer_set_text(text_layer_intro, "Down");
 }
 
 static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer_intro = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_emelia = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_hunter = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(text_layer_intro, "Who are you?");
-  text_layer_set_text(text_layer_emelia, "Emelia");
-  text_layer_set_text(text_layer_hunter, "Hunter");
-  text_layer_set_text_alignment(text_layer_intro, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  layer_add_child(window_layer, text_layer_get_layer(text_layer_emelia));
-  layer_add_child(window_layer, text_layer_get_layer(text_layer_hunter));
+    text_layer_intro = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+    text_layer_emelia = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+    text_layer_hunter = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+  
+    text_layer_set_text(text_layer_intro, "Who are you?");
+    text_layer_set_text(text_layer_emelia, "Emelia");
+    text_layer_set_text(text_layer_hunter, "Hunter");
+    
+    text_layer_set_text_alignment(text_layer_intro, GTextAlignmentCenter);
+    text_layer_set_text_alignment(text_layer_emelia, GTextAlignmentCenter);
+    text_layer_set_text_alignment(text_layer_hunter, GTextAlignmentCenter);
+    
+    layer_add_child(window_layer, text_layer_get_layer(text_layer_intro));
+    layer_add_child(window_layer, text_layer_get_layer(text_layer_emelia));
+    layer_add_child(window_layer, text_layer_get_layer(text_layer_hunter));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+    text_layer_destroy(text_layer_intro);
+    text_layer_destroy(text_layer_emelia);
+    text_layer_destroy(text_layer_hunter);
 }
 
 static void init(void) {
