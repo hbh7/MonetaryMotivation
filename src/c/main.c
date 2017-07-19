3a93bf230644ce4c5a3f9d1608b9c8fa55e357cc
hbh7 <hbh7@hbh7.com>
2017-07-19 12:33:22 -0700

    Removed back button obsolete code

--- src/c/main.c
+++ src/c/main.c
@@ -24,50 +24,19 @@ bool roundPebble;
 
 //Hunter
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Pushed Up");
     window_stack_push(hunterWindow, true);
-    
 }
 
 //Emelia
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-
-    layer_set_hidden((Layer *)text_layer_intro, true);
-    layer_set_hidden((Layer *)text_layer_hunter, true);
-    layer_set_hidden((Layer *)text_layer_emelia, true);
-    
-    text_layer_set_text(text_layer_welcome, "Welcome, Emelia!");
-    text_layer_set_text(text_layer_rate, "Rate: $10.10/hour");
-    text_layer_set_text(text_layer_wages, "Current Wages:");
-    text_layer_set_text(text_layer_wages_2, "$0.00");
-    
-    layer_set_hidden((Layer *)text_layer_welcome, false);
-    layer_set_hidden((Layer *)text_layer_rate, false);
-    layer_set_hidden((Layer *)text_layer_time, false);
-    layer_set_hidden((Layer *)text_layer_wages, false);
-    layer_set_hidden((Layer *)text_layer_wages_2, false);
-
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Pushed Down");
+    window_stack_push(emeliaWindow, true);
 }
-/*
-static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
-    layer_set_hidden((Layer *)text_layer_intro, false);
-    layer_set_hidden((Layer *)text_layer_hunter, false);
-    layer_set_hidden((Layer *)text_layer_emelia, false);
-    
-    layer_set_hidden((Layer *)text_layer_welcome, true);
-    layer_set_hidden((Layer *)text_layer_rate, true);
-    layer_set_hidden((Layer *)text_layer_time, true);
-    layer_set_hidden((Layer *)text_layer_wages, true);
-    layer_set_hidden((Layer *)text_layer_wages_2, true);
-    
-    window_pop_all()
-}
-*/
+
 static void click_config_provider(void *context) {
     window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
     window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-//    window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
 }
 
 static void mainWindow_load(Window *window) {
@@ -192,17 +161,14 @@ static void hunterWindow_load(Window *window) {
     layer_add_child(hunter_window_layer, text_layer_get_layer(text_layer_time));
     layer_add_child(hunter_window_layer, text_layer_get_layer(text_layer_wages));
     layer_add_child(hunter_window_layer, text_layer_get_layer(text_layer_wages_2));
-    
 }
 
 static void hunterWindow_unload(Window *window) {
-    
     text_layer_destroy(text_layer_welcome);
     text_layer_destroy(text_layer_rate);
     text_layer_destroy(text_layer_time);
     text_layer_destroy(text_layer_wages);
     text_layer_destroy(text_layer_wages_2);
-    
 }
 
 static void emeliaWindow_load(Window *window) {
@@ -213,20 +179,12 @@ static void emeliaWindow_load(Window *window) {
     // Pebble Round: 180 x 180
     
     if (roundPebble == true) {  // K so this is left/right, then down/up. Lower # is higher up.
-        text_layer_hunter = text_layer_create(GRect(0, 15, (bounds.size.w), 20));
-        text_layer_emelia = text_layer_create(GRect(0, 140, (bounds.size.w), 20));
-        text_layer_intro = text_layer_create(GRect(0, 80, bounds.size.w, 20));
-        
         text_layer_welcome = text_layer_create(GRect(0, 30, (bounds.size.w), 20));
         text_layer_rate = text_layer_create(GRect(0, 50, (bounds.size.w), 20));
         text_layer_time = text_layer_create(GRect(0, 90, (bounds.size.w), 20));
         text_layer_wages = text_layer_create(GRect(0, 130, (bounds.size.w), 20));
         text_layer_wages_2 = text_layer_create(GRect(0, 150, (bounds.size.w), 20));
-    } else {                    // K so this is left/right, then down/up. Lower # is higher up.
-        text_layer_hunter = text_layer_create(GRect(0, 10, (bounds.size.w-10), 20));
-        text_layer_emelia = text_layer_create(GRect(0, 140, (bounds.size.w-10), 20));
-        text_layer_intro = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-        
+    } else {                    // K so this is left/right, then down/up. Lower # is higher up.        
         text_layer_welcome = text_layer_create(GRect(0, 30, (bounds.size.w-10), 20));
         text_layer_rate = text_layer_create(GRect(0, 50, (bounds.size.w-10), 20));
         text_layer_time = text_layer_create(GRect(0, 90, (bounds.size.w-10), 20));
@@ -234,59 +192,32 @@ static void emeliaWindow_load(Window *window) {
         text_layer_wages_2 = text_layer_create(GRect(0, 150, (bounds.size.w), 20));
     }
     
-    
-    
     text_layer_set_text_alignment(text_layer_welcome, GTextAlignmentCenter);     
     text_layer_set_text_alignment(text_layer_rate, GTextAlignmentCenter);     
     text_layer_set_text_alignment(text_layer_time, GTextAlignmentCenter);     
     text_layer_set_text_alignment(text_layer_wages, GTextAlignmentCenter);     
     text_layer_set_text_alignment(text_layer_wages_2, GTextAlignmentCenter);     
-    
-    
-    text_layer_set_text(text_layer_intro, "Who are you?");
-    text_layer_set_text(text_layer_emelia, "Emelia");
-    text_layer_set_text(text_layer_hunter, "Hunter");
-    
+   
+    text_layer_set_text(text_layer_welcome, "Welcome, Emelia!");
+    text_layer_set_text(text_layer_rate, "Rate: $10.10/hour");
     text_layer_set_text(text_layer_time, "Time Placeholder");
-    
+    text_layer_set_text(text_layer_wages, "Current Wages:");
+    text_layer_set_text(text_layer_wages_2, "$0.00");
 
-    
-    if (roundPebble == true) {
-        text_layer_set_text_alignment(text_layer_emelia, GTextAlignmentCenter);
-        text_layer_set_text_alignment(text_layer_hunter, GTextAlignmentCenter);
-    } else {
-        text_layer_set_text_alignment(text_layer_emelia, GTextAlignmentRight);
-        text_layer_set_text_alignment(text_layer_hunter, GTextAlignmentRight);
-    }
-
-    text_layer_set_text_alignment(text_layer_intro, GTextAlignmentCenter);
-    
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_intro));
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_emelia));
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_hunter));
-    
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_welcome));
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_rate));
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_time));
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_wages));
-    layer_add_child(window_layer, text_layer_get_layer(text_layer_wages_2));
-    
-    layer_set_hidden((Layer *)text_layer_welcome, true);
-    layer_set_hidden((Layer *)text_layer_rate, true);
-    layer_set_hidden((Layer *)text_layer_time, true);
-    layer_set_hidden((Layer *)text_layer_wages, true);
-    layer_set_hidden((Layer *)text_layer_wages_2, true);
+    layer_add_child(emeliaWindow_layer, text_layer_get_layer(text_layer_welcome));
+    layer_add_child(emeliaWindow_layer, text_layer_get_layer(text_layer_rate));
+    layer_add_child(emeliaWindow_layer, text_layer_get_layer(text_layer_time));
+    layer_add_child(emeliaWindow_layer, text_layer_get_layer(text_layer_wages));
+    layer_add_child(emeliaWindow_layer, text_layer_get_layer(text_layer_wages_2));
     
 }
 
 static void emeliaWindow_unload(Window *window) {
-    
     text_layer_destroy(text_layer_welcome);
     text_layer_destroy(text_layer_rate);
     text_layer_destroy(text_layer_time);
     text_layer_destroy(text_layer_wages);
     text_layer_destroy(text_layer_wages_2);
-    
 }
 
 static void init(void) {
